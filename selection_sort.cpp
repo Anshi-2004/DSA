6ad22f64e4503d7ff2e370d9fae6cbf3b253d545
Anshi-2004 <anshikapandey28022004@gmail.com>
2025-11-09 03:32:16 +0530

    selection sort

---
+++ selection_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+selection_sort(int arr[],int n){
+   
+    for(int i=0;i<n-1;i++){
+        int mini=i;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini=j;
+            }
+        }
+        swap(arr[i],arr[mini]);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<=n-1;i++){
+        cin>>arr[i];
+    }
+    selection_sort(arr,n);
+    for (int i=0;i<=n-1;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+}
