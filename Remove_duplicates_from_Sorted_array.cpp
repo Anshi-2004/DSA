69e3b4ec7fabcd49aaac790b7ff2f2e12e2b1da0
Anshi-2004 <anshikapandey28022004@gmail.com>
2025-12-04 14:13:28 +0530

    Remove_duplicates_from_Sorted_array

---
+++ Remove_duplicates_from_Sorted_array.cpp
@@ -0,0 +1,31 @@
+//Remove duplicates from Sorted array
+#include <iostream>
+using namespace std;
+int removeduplicates(int arr[], int n){
+    if (n==0 || n==1){
+        return n;
+    }
+    int j=0;
+    for (int i = 0; i < n-1; i++){
+        if (arr[i] != arr[i+1]){
+            arr[j++] = arr[i];
+        }
+    }
+    arr[j++] = arr[n-1];
+    return j;   
+    
+}
+int main() {
+    int arr[] = {1, 1, 2, 2, 3, 4, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    n = removeduplicates(arr, n);
+
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
