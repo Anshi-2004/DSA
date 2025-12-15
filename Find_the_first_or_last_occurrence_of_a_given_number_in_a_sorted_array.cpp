d7de0a13e8a93ed15cddd5e9311d1457b319ff0b
Anshi-2004 <anshikapandey28022004@gmail.com>
2025-12-15 10:11:18 +0530

    updated

--- Find_the_first_or_last_occurrence_of_a_given_number_in_a_sorted_array.cpp
+++ Find_the_first_or_last_occurrence_of_a_given_number_in_a_sorted_array.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int findFirstOccurrence(const vector<int>& nums, int target) {
+vector<int> searchRange(vector<int>& nums, int target){
     int left = 0, right = nums.size() - 1;
-    int result = -1;
+    int first = -1, last = -1;
+    // Find first occurrence
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (nums[mid] == target) {
-            result = mid;
+            first = mid;
             right = mid - 1; // Continue searching in the left half
         } else if (nums[mid] < target) {
             left = mid + 1;
@@ -16,15 +17,12 @@ int findFirstOccurrence(const vector<int>& nums, int target) {
             right = mid - 1;
         }
     }
-    return result;
-}
-int findLastOccurrence(const vector<int>& nums, int target) {
-    int left = 0, right = nums.size() - 1;
-    int result = -1;
+    left = 0; right = nums.size() - 1;
+    // Find last occurrence
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (nums[mid] == target) {
-            result = mid;
+            last = mid;
             left = mid + 1; // Continue searching in the right half
         } else if (nums[mid] < target) {
             left = mid + 1;
@@ -32,14 +30,21 @@ int findLastOccurrence(const vector<int>& nums, int target) {
             right = mid - 1;
         }
     }
-    return result;
+    return {first, last};
 }
 int main() {
-    vector<int> nums = {1, 2, 2, 2, 3, 4, 5};
-    int target = 2;
-    int firstIndex = findFirstOccurrence(nums, target);
-    int lastIndex = findLastOccurrence(nums, target);
-    cout << "First occurrence of " << target << " is at index: " << firstIndex << endl;
-    cout << "Last occurrence of " << target << " is at index: " << lastIndex << endl;
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+    vector<int> result = searchRange(nums, target);
+    cout << "First and last occurrence of " << target << " is: [" << result[0] << ", " << result[1] << "]" << endl;
+    target = 6;
+    result = searchRange(nums, target);
+    cout << "First and last occurrence of " << target << " is: [" << result[0] << ", " << result[1] << "]" << endl;
+    target = 7;
+    result = searchRange(nums, target);
+    cout << "First and last occurrence of " << target << " is: [" << result[0] << ", " << result[1] << "]" << endl;
+    target = 10;
+    result = searchRange(nums, target);
+    cout << "First and last occurrence of " << target << " is: [" << result[0] << ", " << result[1] << "]" << endl;
     return 0;
 }
